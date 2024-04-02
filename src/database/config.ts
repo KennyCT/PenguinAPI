d7cd9047d790e82ad35b3680860acfe9b760c4fb
Kenny Contreras <kennycontrerastan@gmail.com>
2024-04-02 16:43:05 -0400

    Updated config file
    Updated config file in order to connect to our online database

--- src/database/config.ts
+++ src/database/config.ts
@@ -6,10 +6,10 @@ let password: string = readFileSync('../pw.txt', 'utf-8').trim();
 
 const connection = new Sequelize({
     dialect: "mysql",
-    host: "localhost",
-    username: "root",
+    host: "212.192.28.59",
+    username: "u111397_aTYQzdEwki",
     password: password,
-    database: "penguin",
+    database: "s111397_penguin",
     logging: false,
     models: [Penguin]
 });
